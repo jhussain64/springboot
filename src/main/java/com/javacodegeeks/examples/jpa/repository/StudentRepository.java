c4f56135182a46ea06c2027c7e1f22091441dd97
jhussain64 <35588826+jhussain64@users.noreply.github.com>
2021-09-26 19:28:08 +0500

    Delete src/main directory

--- src/main/java/com/javacodegeeks/examples/jpa/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javacodegeeks.examples.jpa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.javacodegeeks.examples.jpa.model.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
